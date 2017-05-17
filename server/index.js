66250c97b8dfb138f707a48b10976c533349ceeb
Trachifab <fabiendorey@gmail.com>
2017-05-17 16:43:57 +0200

    Creating user Schema

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const http  = require('http');
 const bodyParser  = require('body-parser');
 const morgan  = require('morgan');
+const router = require('./router');
+const mongoose = require('mongoose');
+
+//DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 const app = express();
 
 //App setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 //Server Setup
 const port = process.env.PORT || 3090;
